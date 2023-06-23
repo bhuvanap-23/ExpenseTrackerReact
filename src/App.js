9ac60c7f9fac431be0ddc930d3cdccf174a075e3
Bhuvana P <bhuvanap263@gmail.com>
2023-06-23 00:17:35 -0700

    First commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+import Header from './components/Header'
+import Balance from './components/Balance'
+import IncomeExpenses from './components/IncomeExpenses';
+import TransactionList from './components/TransactionList';
+import AddTransaction from './components/AddTransaction';
+import { GlobalProvider } from './context/GlobalState';
+
+function App() {
+  return (
+    <div>
+      <Header/>
+      <div className="container">
+        <GlobalProvider>
+          <Balance/>
+          <IncomeExpenses/>
+          <TransactionList/>
+          <AddTransaction/>
+        </GlobalProvider>
+      </div>
+    </div>
+  );
+}
+
+export default App;
